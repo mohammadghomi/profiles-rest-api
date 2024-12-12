5eaf4465ba9050e677d6b261ccdf38c2c14bef2e
mohammad <mhamdamn456@gmail.com>
2024-12-12 08:37:07 -0800

    added method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+from profiles_api import serializers
 
 class HelloApiView(APIView):
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         an_apiview = [
@@ -11,4 +14,25 @@ class HelloApiView(APIView):
         'Gives you must control over you application logic',
         'Is mapped manually to URLs'
         ]
-        return Response({'masage': 'Hello!', 'an_apiview': an_apiview})
+        return Response({'massage': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            massage = f"hello {name}"
+            return Response({'massage': massage})
+        else:
+           return Response(
+               serializer.errors,
+               status=status.HTTP_400_BAD_REQUEST
+           )
+
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
